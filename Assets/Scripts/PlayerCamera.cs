b976fd9ce75202c0b5a1d83b0c05b79d9a0372a1
Erik Batlle Clavero <erikbc_95@hotmail.com>
2017-10-23 02:08:49 +0200

    added Box Network implementation with camera instantiation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerCamera : MonoBehaviour
 {
 
-    public GameObject player;       //Public variable to store a reference to the player game object
+	public GameObject player;      //Public variable to store a reference to the player game object
 
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
@@ -12,14 +12,23 @@ public class PlayerCamera : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-    }
+		
+		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+		offset = transform.position;
+		print (offset);
+	}
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+		if (player == null) {
+			player = GameObject.Find("CharacterRobotBoy(Clone)");
+			offset = transform.position - player.transform.position;
+		} 
+		else {
+			transform.position = player.transform.position + offset;
+		}
     }
 }
--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -39,4 +39,5 @@ namespace UnityStandardAssets._2D
             m_Jump = false;
         }
 			    
+	}
 }
