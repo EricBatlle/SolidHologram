f6b1558ccebc2c02c9eb054139b01bdbf7de1298
airhune <ls27346@salleurl.edu>
2017-10-27 19:35:22 +0200

    alun intervention 2.0

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerCamera : MonoBehaviour
 {
 
-	public GameObject player;      //Public variable to store a reference to the player game object
+	GameObject player;      //Public variable to store a reference to the player game object
 
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
@@ -23,12 +23,19 @@ public class PlayerCamera : MonoBehaviour
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-		if (player == null) {
-			player = GameObject.Find("CharacterRobotBoy(Clone)");
+	/*	if (player == null) {
+			//player = GameObject.Find("CharacterRobotBoy(Clone)");
+			//player = GameObject.CompareTag ("Player");
 			offset = transform.position - player.transform.position;
 		} 
 		else {
 			transform.position = player.transform.position + offset;
-		}
+		}*/
+		if (player != null)
+			transform.position = player.transform.position + offset;
     }
+
+	public void setPlayer(GameObject playerPassed) {
+		player = playerPassed;
+	}
 }
---
+++ Assets/checkServer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+using UnityEngine;
+
+public class checkServer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isServer){
+			print("I'm the server");
+		}
+
+		if(!isServer){
+			print("I'm the client");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
