9ac14c12ce009e54a146902ee95448bc49f047fd
ls29322 <erikbc_95@hotmail.com>
2018-02-23 17:18:19 +0100

    added box sync animatino between clients

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -166,6 +166,41 @@ namespace UnityStandardAssets._2D
 
         private void Flip()
         {
+            if (isServer)
+            {
+                RpcFlip();
+            }
+            else
+            {
+                NmFlip();
+                CmdFlip();
+            }
+        }
+
+        private void NmFlip()
+        {
+            // Switch the way the player is labelled as facing.
+            m_FacingRight = !m_FacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+        [ClientRpc]
+        private void RpcFlip()
+        {
+            // Switch the way the player is labelled as facing.
+            m_FacingRight = !m_FacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+        [Command]
+        private void CmdFlip()
+        {
             // Switch the way the player is labelled as facing.
             m_FacingRight = !m_FacingRight;
 
