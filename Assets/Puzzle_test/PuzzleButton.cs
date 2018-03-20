00cb2c933acb879c2f3452b8e6854da75d2250e9
ls29322 <erikbc_95@hotmail.com>
2018-03-20 12:31:58 +0100

    trying to fix problem with prefab with more than one network identity on childs

---
+++ Assets/Puzzle_test/PuzzleButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PuzzleButton : NetworkBehaviour, IInteractable {
+    
+    [SerializeField] private Material[] materials;
+    private int materialsCount = 0;
+
+    public Material currMaterial;
+    public event Action OnColorChange;
+   
+
+    private void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.CompareTag ("Player") || collision.gameObject.CompareTag ("line")) {
+            PlayerInteraction();
+		}
+	}
+    
+    public void PlayerInteraction()
+    {
+        if (isServer)
+        {
+            RpcNextColor();
+        }
+        else
+        {
+            CmdNextColor(); 
+        }
+    }
+
+    //Set Next Color
+    private void nextColor()
+    {
+        if (materialsCount >= materials.Length)
+        {
+            materialsCount = 0;
+        }
+        this.GetComponent<MeshRenderer>().material = materials[materialsCount];
+        currMaterial = materials[materialsCount];
+        materialsCount++;
+
+        OnColorChange();
+    }
+
+    [Command]
+    private void CmdNextColor()
+    {
+        RpcNextColor(); 
+    }
+    [ClientRpc]
+    private void RpcNextColor()
+    {
+        nextColor();
+    }
+}
---
+++ Assets/Puzzle_test/PuzzleColorSystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleColorSystem : MonoBehaviour {
+
+    [SerializeField] private PuzzleButton[] puzzleButtons;
+    [SerializeField] private Material materialSolution;
+    [SerializeField] private Mover puzzleDoorMover;
+
+    public event Action OnPuzzleSolved;
+    public event Action OnPuzzleNotSolved;
+
+    private bool puzzleSolved = false;
+    private bool puzzleHasBeenSolvedOnce = false;
+
+    private void OnEnable()
+    {
+        foreach (PuzzleButton puzzleButton in puzzleButtons)
+        {
+            puzzleButton.OnColorChange += checkSolution;
+        }
+        this.OnPuzzleSolved += puzzleDoorMover.Open;
+        this.OnPuzzleNotSolved += puzzleDoorMover.Close;
+    }
+
+    private void OnDisable()
+    {
+        foreach (PuzzleButton puzzleButton in puzzleButtons)
+        {
+            puzzleButton.OnColorChange -= checkSolution;
+        }
+        this.OnPuzzleSolved -= puzzleDoorMover.Open;
+        this.OnPuzzleNotSolved -= puzzleDoorMover.Close;
+    }
+
+    private void checkSolution()
+    {
+        puzzleSolved = true;
+        foreach (PuzzleButton puzzleButton in puzzleButtons)
+        {
+            if (puzzleButton.currMaterial != materialSolution)
+            {
+                puzzleSolved = false;
+            }
+        }
+
+        if (puzzleSolved == true)
+        {
+            puzzleHasBeenSolvedOnce = true;
+            OnPuzzleSolved();            
+        }
+        else if(puzzleHasBeenSolvedOnce)
+        {
+            OnPuzzleNotSolved();
+        }
+    }
+    
+}
