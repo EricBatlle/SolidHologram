7fc7288b53a3015261dcf5b1cd36b54c23b10b85
Eric <erikbc_95@hotmail.com>
2018-04-04 00:50:11 +0200

    added MKGLOW and enemyCamera prefab

---
+++ Assets/Scripts/Enemies/CameraEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnemy : MonoBehaviour {
+
+    [SerializeField] private HingeJoint2D joint;
+    [SerializeField] private float waitingTime = 2;     //Time that camera awaits until change direction
+
+    private bool changingDirection = false;
+
+    // Update is called once per frame
+    void Update () {
+
+        if ((joint.limitState != JointLimitState2D.Inactive) && (!changingDirection) )
+        {
+            changingDirection = true;
+            Invoke("changeMotorDirection", waitingTime);
+        }
+    }
+    
+    private void changeMotorDirection()
+    {
+        JointMotor2D motor = joint.motor;
+        motor.motorSpeed = joint.motor.motorSpeed * -1;        
+        joint.motor = motor;
+
+        changingDirection = false;
+    }
+
+    private void stopMotor()
+    {
+        JointMotor2D motor = joint.motor;
+        motor.motorSpeed = 0;
+
+        joint.motor = motor;
+    }
+}
