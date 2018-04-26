d8a3ea17e813157ea36f4b2e10fa4b862dadbdf4
Eric <erikbc_95@hotmail.com>
2018-04-26 21:39:42 +0200

    added sensor new behaviour (burns drops, and flashes when do that)

---
+++ Assets/Scripts/Enemies/Sensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+
+    //Matar lo que esté en su rango (en su defecto, las cosas mueren al entrar en contacto con él)
+    //Cambiar de sprite cada vez que lo haga
+
+    [SerializeField] private Sprite spriteOn;
+    [SerializeField] private Sprite spriteOff;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {        
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteOn;
+        StartCoroutine(SetInitialSpriteAfter(0.1f));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {        
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteOn;
+        StartCoroutine(SetInitialSpriteAfter(0.1f));    
+    }
+
+    IEnumerator SetInitialSpriteAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteOff;
+    }
+}
---
+++ Assets/Scripts/Environment/spriteWhenTag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteWhenTag : MonoBehaviour {
+
+    public Sprite sprite;
+    public string[] triggerTags;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (string tag in triggerTags)
+        {
+            if ((collision.gameObject.CompareTag(tag)))
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+                StartCoroutine(DestroyAfterTime(0.1f));
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (string tag in triggerTags)
+        {
+            if ((collision.gameObject.CompareTag(tag)))
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+                StartCoroutine(DestroyAfterTime(0.1f));
+
+            }
+        }
+    }
+
+    IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
